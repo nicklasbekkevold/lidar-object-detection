791ba691cf0bf4917cb95ad25922944d94d5da45
44287251+nicklasbekkevold@users.noreply.github.com <nicklasbekkevold@gmail.com>
2021-11-29 17:47:59 +0100

    Make rgb merger script and run

--- src/dataset_builder.py
+++ src/dataset_builder.py
@@ -1,5 +1,9 @@
-import cv2
 import os
+import sys
+
+import cv2
+
+from image_merger import greyscale_to_rgb
 
 
 class DatasetBuilder:
@@ -21,6 +25,36 @@ class DatasetBuilder:
             success, image = vidcap.read()
             count += 1
         print('Finished converting:', parsed_file_name)
+
+    def convert_with_merge(self, video_number, video_files):
+        parsed_file_name = video_files[0].split('/')[-1].split('_')[0]
+        ambient_capture, intensity_capture, range_capture = map(
+            cv2.VideoCapture, sorted(video_files))
+
+        count = 0
+        success_ambient, ambient = ambient_capture.read()
+        success_intensity, intensity = intensity_capture.read()
+        success_range, range = range_capture.read()
+        success = success_ambient and success_intensity and success_range
+        while success:
+
+            framecount = '{number:06}'.format(number=count)
+            file_name_image = f'data/all_videos_merged/images/{parsed_file_name}_frame_{framecount}.jpg'
+            file_name_label = f'data/all_videos_merged/labels/{parsed_file_name}_frame_{framecount}.txt'
+
+            # save frame as JPEG file
+            greyscale_to_rgb(ambient, intensity, range, file_name_image)
+            os.popen(
+                f'cp {self.label_paths[video_number][count]} {file_name_label}')
+
+            success_ambient, ambient = ambient_capture.read()
+            success_intensity, intensity = intensity_capture.read()
+            success_range, range = range_capture.read()
+
+            success = success_ambient and success_intensity and success_range
+            count += 1
+
+        print('Finished converting and merging:', parsed_file_name)
 
     def parse_video_label_directories(self):
         # Fetch labeled video numbers
@@ -57,25 +91,31 @@ class DatasetBuilder:
             if video_number in self.video_numbers_sorted:
                 self.video_paths[video_number].append(os.path.join(path, name))
 
-    def make_dirs_if_not_exists(self):
+    def make_dirs_if_not_exists(self, folder_name):
         try:
-            os.makedirs(f"data/all_videos/images")
-            os.mkdir(f"data/all_videos/labels")
-        except OSError as error:
+            os.makedirs(f'data/{folder_name}/images')
+            os.mkdir(f'data/{folder_name}/labels')
+        except OSError:
             pass
 
-    def build(self):
+    def build(self, merge_channels=False):
+        folder_name = 'all_videos_merged' if merge_channels else 'all_videos'
+
         self.parse_video_label_directories()
         self.get_num_of_label_files()
         self.generate_label_paths()
         self.get_video_paths()
-        self.make_dirs_if_not_exists()
+        self.make_dirs_if_not_exists(folder_name)
 
         for video_number, video_files in self.video_paths.items():
-            for video_file in video_files:
-                self.convert(video_number, video_file)
+            if merge_channels:
+                self.convert_with_merge(video_number, video_files)
+            else:
+                for video_file in video_files:
+                    self.convert(video_number, video_file)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    merge_channels = len(sys.argv) > 1 and sys.argv[1] == '--merge'
     dataset_builder = DatasetBuilder()
-    dataset_builder.build()
+    dataset_builder.build(merge_channels=merge_channels)
